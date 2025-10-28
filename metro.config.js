82a50e40f04f626908c8b773f1165488c47939ca
Tang Ying Hao <yinghaotyh963@gmail.com>
2025-10-29 04:28:53 +0800

    feat: added svg as component functionality, added NTULogo SVG & PNG

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,11 @@ const { withNativeWind } = require('nativewind/metro');
  
 const config = getDefaultConfig(__dirname)
  
-module.exports = withNativeWind(config, { input: './global.css' })
+// SVG -> component transformer
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+
+// Make Metro treat .svg as source, not asset
+config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== 'svg');
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+
+module.exports = withNativeWind(config, { input: './global.css' });
